0b74618771fcd05bbe39a2ca257e47f0a5973523
송하우 <song03621@gmail.com>
2026-05-11 23:47:23 +0900

    feat(ui): DefeatView — DEFEAT 패널 ViewBase 구현

---
+++ OUD/Assets/Scripts/Unity/Battle/View/DefeatView.cs
@@ -0,0 +1,29 @@
+using System;
+using OUD.Unity.Common;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace OUD.Unity.Battle.View
+{
+    public class DefeatView : ViewBase
+    {
+        [SerializeField] private TMP_Text _titleText;
+        [SerializeField] private TMP_Text _reachedNodeText;
+        [SerializeField] private Button   _restartButton;
+
+        public event Action OnRestartClicked;
+
+        private void Awake()
+        {
+            if (_restartButton != null)
+                _restartButton.onClick.AddListener(() => OnRestartClicked?.Invoke());
+        }
+
+        public void SetDefeatInfo(int reachedNode, int totalNodes)
+        {
+            if (_reachedNodeText != null)
+                _reachedNodeText.text = $"도달 노드: {reachedNode}/{totalNodes}";
+        }
+    }
+}
